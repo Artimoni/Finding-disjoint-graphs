a710c6154bfe6d679cd45ab196bc9bdf11134f9c
Artimoni <76207858+Artimoni@users.noreply.github.com>
2025-05-05 16:43:04 +0800

    Update graph.h

--- disjoint_graphs/graph.h
+++ disjoint_graphs/graph.h
@@ -4,29 +4,31 @@
 #include <iostream>
 #include <fstream>
 #include <set>
-#include <queue>
 #include <string>
-#include <sstream>
-#include <memory>
+#include <sstream> //Удалены нерабочие библиотеки(<queue>)
 
 class Node {
 public:
-    std::string name;
-    std::set<std::shared_ptr<Node>> neighbours;
-    Node(const std::string& name) : name(name) {}
+    std::string name;                     
+    std::set<Node*> neighbours;
+
+    Node(const std::string& name) : name(name) {}  
+    ~Node() = default;  //Конструктор и Деконструктор
 };
 
 class Graph {
 private:
-    std::set<std::shared_ptr<Node>> nodes;
+    std::set<Node*> nodes;  //Все узлы
 public:
-    void addEdge(const std::shared_ptr<Node>& a, const std::shared_ptr<Node>& b);
-    std::shared_ptr<Node> getOrCreateNode(const std::string& name);
-    std::vector<std::set<std::shared_ptr<Node>>> findConnectedComponents();
-    void clear();
+    ~Graph() { clear(); }  //Ручная отчистка
+
+    void addEdge(Node* a, Node* b);  
+    Node* getOrCreateNode(const std::string& name);  
+    std::set<std::set<Node*>> findConnectedComponents();  
+    void clear(); 
 };
 
-Graph readGraphFromFile(const std::string& filename);
-void saveComponentToFile(const std::set<std::shared_ptr<Node>>& component, int index);
+Graph readGraphFromFile(const std::string& filename); 
+void saveComponentToFile(const std::set<Node*>& component, int index); 
 
 #endif
